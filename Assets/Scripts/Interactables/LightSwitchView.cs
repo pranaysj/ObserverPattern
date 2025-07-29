7f6a7b359f14335f313dfb7c56b48f755e1a659b
pranaysj <pranaysj3@gmail.com>
2025-07-29 18:31:48 +0530

    Converting delegates into the event
    Convert delegate keyword by Action
    Do respected changes in listeners

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using static LightSwitchView;
@@ -6,16 +7,16 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public delegate void LightSwitchDelegate();
-    public static event LightSwitchDelegate lightToggled;
 
-    private void OnEnable() => lightToggled += onLightSwitch;
+    public static event Action OnLightSwitchToggle;
 
-    private void OnDisable() => lightToggled -= onLightSwitch;
+    private void OnEnable() => OnLightSwitchToggle += onLightSwitch;
+
+    private void OnDisable() => OnLightSwitchToggle -= onLightSwitch;
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => lightToggled?.Invoke();
+    public void Interact() => OnLightSwitchToggle?.Invoke();
 
     private void toggleLights()
     {
