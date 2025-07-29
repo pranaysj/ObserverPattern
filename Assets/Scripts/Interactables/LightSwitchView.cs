d1f31360e63f5b1847b24089995c04791af6bee4
pranaysj <pranaysj3@gmail.com>
2025-07-29 12:55:32 +0530

    Creating LightSwitch Delegate
    Create a delegate inside LightSwitchView.
    Do Invocation.
    Add Listener of LightSwitch Delegate inside LightSwitchView

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -6,11 +6,20 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
+    public delegate void LightSwitchDelegates();
+    public LightSwitchDelegates lightSwitch;
+
+    private void OnEnable()
+    {
+        lightSwitch = LightSwitchToggle;
+    }
+
     private void Start() => currentState = SwitchState.Off;
+
 
     public void Interact()
     {
-        //Todo - Implement Interaction
+        lightSwitch?.Invoke();
     }
     private void toggleLights()
     {
@@ -33,5 +42,13 @@ public class LightSwitchView : MonoBehaviour, IInteractable
         {
             lightSource.enabled = lights;
         }
+    }
+
+    private void LightSwitchToggle()
+    {
+        toggleLights();
+        GameService.Instance.GetInstructionView().HideInstruction();
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
+
     }
 }
