821d07044989787acae27df7118fa11668388073
pranaysj <pranaysj3@gmail.com>
2025-07-30 10:58:01 +0530

    Adding OnKeyPickedUp Listner in PlayerController
    Add Listener of OnKeyPickedUp in PlayerController
    Increment keys in PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,11 +27,13 @@ public class PlayerController
         playerState = PlayerState.InDark;
 
         EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnKeyPickedUp.AddListener(onKeyPickUp);
     }
 
     ~PlayerController()
     {
         EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnKeyPickedUp.RemoveListener(onKeyPickUp);
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
@@ -84,5 +86,11 @@ public class PlayerController
             PlayerState = PlayerState.None;
         else
             PlayerState = PlayerState.InDark;
+    }
+
+
+    private void onKeyPickUp(int keys)
+    {
+        KeysEquipped = keys;
     }
 }
